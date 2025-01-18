44aadfcc35bb7f76681e9a8bd5ceb012cae37fc4
akshaldhal <86314014+akshaldhal@users.noreply.github.com>
2025-01-18 01:57:11 -0500

    Made the video printing method

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
 import os
 import sys
 import cv2
+import time
 EXECUTABLE_NAME : str = 'term-player'
-HEIGHT = 32
+HEIGHT = 64
+
+def __print_frames(frames : list, fps : float) -> None:
+  for frame in frames:
+    for row in frame:
+      for pixel in row:
+        r, g, b = pixel
+        color = 16 + (36 * (r // 51)) + (6 * (g // 51)) + (b // 51)
+        print(f"\033[48;5;{color}m \033[0m", end="")
+      print()
+    print("\033[0m", end="")
+    time.sleep(1 / fps)
+    print("\033[H", end="")
 
 def __preprocess_video__(filepath : str) -> None:
-  # convert video to frames and save array of downscaled pixel values from each frame to a file
   cap = cv2.VideoCapture(filepath)
   if not cap.isOpened():
     print('Error: Could not open video')
@@ -15,18 +27,21 @@ def __preprocess_video__(filepath : str) -> None:
   fps = cap.get(cv2.CAP_PROP_FPS)
   height = HEIGHT
   width = int(_width * height / _height)
-  frame_count = 0
-  output_dir = 'frames.temp'
-  os.makedirs(output_dir, exist_ok=True)
+  # frame_count = 0
+  # output_dir = 'frames.temp'
+  # os.makedirs(output_dir, exist_ok=True)
+  frame_array = []
   while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
       break
     frame = cv2.resize(frame, (width, height))
-    frame_filename = os.path.join(output_dir, f"frame_{frame_count:04d}.jpg")
-    cv2.imwrite(frame_filename, frame)
-    frame_count += 1
-    
+    frame_array.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    # frame_filename = os.path.join(output_dir, f"frame_{frame_count:04d}.jpg")
+    # cv2.imwrite(frame_filename, frame)
+    # frame_count += 1
+  cap.release()
+  __print_frames(frame_array, fps)  
 
 def main(filepath : str) -> None:
   if filepath.endswith('.mp4'):
