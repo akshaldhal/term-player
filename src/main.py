2f64783997ec7054d71d5800dcdad6bccd367496
akshaldhal <86314014+akshaldhal@users.noreply.github.com>
2025-01-17 23:50:27 -0500

    New requirements.txt, scaled frame extraction

--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
 import os
 import sys
+import cv2
 EXECUTABLE_NAME : str = 'term-player'
+HEIGHT = 32
 
 def __preprocess_video__(filepath : str) -> None:
   # convert video to frames and save array of downscaled pixel values from each frame to a file
-  raise NotImplementedError("Not implemented yet")
+  cap = cv2.VideoCapture(filepath)
+  if not cap.isOpened():
+    print('Error: Could not open video')
+    sys.exit(1)
+  _width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+  _height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+  fps = cap.get(cv2.CAP_PROP_FPS)
+  height = HEIGHT
+  width = int(_width * height / _height)
+  frame_count = 0
+  output_dir = 'frames.temp'
+  os.makedirs(output_dir, exist_ok=True)
+  while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+      break
+    frame = cv2.resize(frame, (width, height))
+    frame_filename = os.path.join(output_dir, f"frame_{frame_count:04d}.jpg")
+    cv2.imwrite(frame_filename, frame)
+    frame_count += 1
+    
 
 def main(filepath : str) -> None:
   if filepath.endswith('.mp4'):
